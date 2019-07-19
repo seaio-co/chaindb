84f0fa62e29138a899cb735e28a9eab3862661bc
wuxiaoyong <wuxiaoyong@blhcn.com>
2019-07-19 14:33:10 +0800

    clear up memorydb

--- memorydb/memorydb_test.go
+++ memorydb/memorydb_test.go
@@ -26,12 +26,11 @@ func TestMemDB_PutGet(t *testing.T) {
 func TestMemDB_Has(t *testing.T) {
 	// 初始化数据库
 	db := Init()
-
 	if err := db.Put(key, value); err != nil {
 		panic("存储失败：" + err.Error())
 	}
 
-	if ok,_ := db.Has(key); ok {
+	if ok, _ := db.Has(key); ok {
 		t.Log("当前键值存在")
 	} else {
 		t.Log(ok)
@@ -42,17 +41,15 @@ func TestMemDB_Has(t *testing.T) {
 func TestMemDB_Del(t *testing.T) {
 	// 初始化数据库
 	db := Init()
-
 	if err := db.Put(key, value); err != nil {
 		panic("存储失败：" + err.Error())
 	}
 
 	db.Del(key)
-
 	values, err := db.Get(key)
 	if err != nil {
 		t.Logf("获取失败：" + err.Error())
-	}else {
+	} else {
 		t.Logf("获取成功：%s", string(values))
 	}
 }
@@ -60,10 +57,8 @@ func TestMemDB_Del(t *testing.T) {
 func TestMemDB_GetAllKey(t *testing.T) {
 	// 初始化数据库
 	db := Init()
-
 	db.Put(key, value)
 	db.Put([]byte("111111"), value)
-
 	keys := db.GetAllKey()
 	for _, v := range keys {
 		t.Logf(string(v))
